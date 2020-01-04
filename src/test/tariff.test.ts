9de868dc89ade7868d10117feec06be9fc3742d4
Matheus <matheusdsb@gmail.com>
2020-01-04 15:29:49 -0300

    implementing test cases

---
+++ src/test/tariff.test.ts
@@ -0,0 +1,40 @@
+import { expect } from 'chai';
+import { Tariff } from '../model/tariff.model';
+import { BasicElectricityTariff } from '../model/basic-electricity-tariff.model';
+import { PackagedTariff } from '../model/packaged-tariff.model';
+
+describe('BasicElectricityTariff.annualCost', () => {
+
+    it('Consumption = 3500 => Should return 830', () => {
+        const basicElectricityTariff : Tariff = new BasicElectricityTariff(3500);
+        expect(basicElectricityTariff.annualCost).to.equal(830);
+    });
+
+    it('Consumption = 4500 => Should return 1050', () => {
+        const basicElectricityTariff : Tariff = new BasicElectricityTariff(4500);
+        expect(basicElectricityTariff.annualCost).to.equal(1050);
+    });
+    
+    it('Consumption = 6000 => Should return 1380', () => {
+        const basicElectricityTariff : Tariff = new BasicElectricityTariff(6000);
+        expect(basicElectricityTariff.annualCost).to.equal(1380);
+    });    
+});
+
+describe('PackagedTariff.annualCost', () => {
+
+    it('Consumption = 3500 => Should return 800', () => {
+        const packagedTariff : Tariff = new PackagedTariff(3500);
+        expect(packagedTariff.annualCost).to.equal(800);
+    });  
+
+    it('Consumption = 4500 => Should return 950', () => {
+        const packagedTariff : Tariff = new PackagedTariff(4500);
+        expect(packagedTariff.annualCost).to.equal(950);
+    });  
+    
+    it('Consumption = 6000 => Should return 1400', () => {
+        const packagedTariff : Tariff = new PackagedTariff(6000);
+        expect(packagedTariff.annualCost).to.equal(1400);
+    });      
+});
